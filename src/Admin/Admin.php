745adf69360859d8f52a61f189000ed6867ccf46
wpfresher <kawsar.ahmed198133@gmail.com>
2024-02-11 21:39:46 +0600

    Include iris js from wordpress

--- src/Admin/Admin.php
+++ src/Admin/Admin.php
@@ -19,6 +19,7 @@ class Admin {
 	 */
 	public function __construct() {
 		add_action( 'init', array( $this, 'init' ), 1 );
+		add_action( 'admin_enqueue_scripts', array( $this, 'admin_scripts' ) );
 	}
 
 	/**
@@ -29,5 +30,22 @@ class Admin {
 	public function init() {
 		utm_source_tracker()->services->add( Settings::instance() );
 		utm_source_tracker()->services->add( Menus::class );
+	}
+
+	/**
+	 * Enqueue admin scripts.
+	 *
+	 * @param string $hook Hook name.
+	 *
+	 * @since 1.0.0
+	 */
+	public function admin_scripts( $hook ) {
+		wp_enqueue_script(
+			'iris',
+			admin_url( 'js/iris.min.js' ),
+			array( 'jquery-ui-draggable', 'jquery-ui-slider', 'jquery-touch-punch' ),
+			false,
+			1
+		);
 	}
 }
