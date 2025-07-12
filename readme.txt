3a2f95c5810280989d5d0067086ecac0898ddda9
kawsarahmedr <kawsar.ahmed198133@gmail.com>
2025-07-12 11:04:19 +0600

    Prepare Release v1.2.3

--- readme.txt
+++ readme.txt
@@ -1,9 +1,9 @@
-=== UTM Manager - The Best UTM Leads Tracker ===
+=== UTM Manager ===
 Contributors: urldev
 Tags: utm manager, utm tracker, utm, analytics, insights
 Requires at least: 5.0
 Tested up to: 6.8
-Stable tag: 1.2.2
+Stable tag: 1.2.3
 Requires PHP: 7.4
 License: GPLv2 or later
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
@@ -63,6 +63,10 @@ UTM Manager is integration-ready and compatible with popular analytics tools, al
 3. Settings Page
 
 == Changelog ==
+= 1.2.3 (12 July, 2025) =
+* Update: Update the plugin name to match the plugin slug.
+* Compatibility: Compatible with the WordPress latest version.
+
 = 1.2.2 (01 June 2025) =
 * Fix: Fixed few known issues.
 
--- utm-manager.php
+++ utm-manager.php
@@ -1,13 +1,13 @@
 <?php
 /**
  * Plugin Name:       UTM Manager
- * Plugin URI:        https://wpfreshers.com/plugins/utm-manager/
+ * Plugin URI:        https://urldev.com/plugins/utm-manager/
  * Description:       UTM Manager is a powerful and user-friendly WordPress plugin designed to help you efficiently track and manage UTM parameters across your website. With UTM Manager, you can effortlessly monitor the performance of your marketing campaigns, understand the source of your traffic, and gain valuable insights to optimize your strategies.
- * Version:           1.2.2
+ * Version:           1.2.3
  * Requires at least: 5.0
  * Requires PHP:      7.4
- * Author:            WpFreshers
- * Author URI:        https://wpfreshers.com
+ * Author:            UrlDev
+ * Author URI:        https://urldev.com
  * License:           GPL v2 or later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       utm-manager
@@ -41,7 +41,7 @@ require_once __DIR__ . '/vendor/autoload.php';
  * @return Plugin
  */
 function utm_manager() {
-	return Plugin::create( __FILE__, '1.2.2' );
+	return Plugin::create( __FILE__, '1.2.3' );
 }
 
 // Initialize the plugin.
