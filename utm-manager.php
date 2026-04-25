c69bb0357cb4af3a0c09732554a4ac4081315928
kawsarahmedr <kawsar.ahmed198133@gmail.com>
2026-04-25 23:50:35 +0600

    Prepare release v1.3.1

--- utm-manager.php
+++ utm-manager.php
@@ -3,20 +3,20 @@
  * Plugin Name:       UTM Manager
  * Plugin URI:        https://urldev.com/plugins/utm-manager/
  * Description:       UTM Manager is a powerful and user-friendly WordPress plugin designed to help you efficiently track and manage UTM parameters across your website. With UTM Manager, you can effortlessly monitor the performance of your marketing campaigns, understand the source of your traffic, and gain valuable insights to optimize your strategies.
- * Version:           1.3.0
+ * Version:           1.3.1
  * Requires at least: 5.0
  * Requires PHP:      7.4
+ * Tested up to:      6.9
  * Author:            UrlDev
  * Author URI:        https://urldev.com
  * License:           GPL v2 or later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       utm-manager
  * Domain Path:       /languages
- * Tested up to:      6.9
  *
  * @package UTMManager
  *
- * UTM Manager is free software: you can redistribute it and/or modify
+ * UTM Manager is a free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 2 of the License, or
  * any later version.
@@ -25,26 +25,23 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with UTM Manager. If not, see https://www.gnu.org/licenses/gpl-2.0.html
  */
 
 use UTMManager\Plugin;
 
 defined( 'ABSPATH' ) || exit; // Exit if accessed directly.
 
-// Autoload classes.
+// Autoload optimized classes.
 require_once __DIR__ . '/vendor/autoload.php';
 
 /**
  * Get the plugin instance.
  *
  * @since 1.0.0
- * @return Plugin plugin initialize class.
+ * @return Plugin The plugin instance.
  */
 function utm_manager() {
-	return Plugin::create( __FILE__, '1.3.0' );
+	return Plugin::create( __FILE__, '1.3.1' );
 }
 
 // Initialize the plugin.
